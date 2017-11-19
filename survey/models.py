35f223d3797e02dc50c748bd11a88878ae89bc22
Laine <lum202@nyu.edu>
2017-11-19 17:35:07 +0400

    statement update

--- survey/models.py
+++ survey/models.py
@@ -16,6 +16,18 @@ class Constants(BaseConstants):
     players_per_group = None
     num_rounds = 1
 
+    # From a scale of 1 to 7 (1 = completely disagree; 4 = completely agree), please tell us how much you agree with the following statements:
+    # Randomize order of rows
+    ChoiceTable2 = {
+         'q_CT2_publicsector':'I would rather work in public than private sector.',
+         'q_CT2_familyimportance':'Family is more important than work.',
+         'q_CT2_wagevshours':'I would work for higher wage even if there is less flexibility in working hours.',
+         'q_CT2_incomecontribution':'Having a job is the best way for a woman to be an independent person.',
+         'q_CT2_jobwomenindependence':'When a mother works for pay outside the home, the children suffer.',
+         'q_CT2_workingmother':'A university education is more important for a boy than for a girl.',
+         'q_CT2_fatherSuited':'Being a housewife is just as fulfilling as working for pay.',
+         'q_CT2_preschoolWork':'Women are encouraged to study at the university.',
+    } #see for loop below class
 
 class Subsession(BaseSubsession):
     pass
