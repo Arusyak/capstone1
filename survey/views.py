4f24509b61909a0879c6f5d22125aba85c8b72f7
Laine <lum202@nyu.edu>
2017-11-21 17:37:33 +0400

    add gpa, major declaration, profitability and statements

--- survey/views.py
+++ survey/views.py
@@ -33,6 +33,80 @@ class MathLevel(Page):
     form_fields = ['q_mathlevel']
 
 
+class MajorDeclarationYN(Page):
+    form_model=models.Player
+
+    form_fields = ['q_majordeclarationYN']
+
+
+class ExpectedStudyTrack(Page):
+    form_model=models.Player
+    form_fields = ['q_expectedstudytrack']
+
+    def is_displayed(self):
+        return self.player.q_majordeclarationYN == "No"
+
+
+class DeclaredStudyTrack(Page):
+    form_model=models.Player
+    form_fields = ['q_declaredstudytrack']
+
+    def is_displayed(self):
+        return self.player.q_majordeclarationYN == "Yes"
+
+
+class GPAStudyTrack(Page):
+    form_model=models.Player
+
+    form_fields = ['q_gpastudytrack']
+
+
+class ProfitableStudyTrack(Page):
+    form_model=models.Player
+
+    form_fields = ['q_profitablestudytrack']
+
+
+# class MathLevelNYUAD(Page):
+#     form_model=models.Player
+#
+#     form_fields = ['q_mathlevelnyuad']
+#
+#
+# class PassingGradeMath(Page):
+#     form_model=models.Player
+#
+#     form_fields = ['q_passinggrade']
+#
+#
+# class Wealth(Page):
+#     form_model=models.Player
+#
+#     form_fields = ['q_wealth']
+#
+#
+class Statements1(Page):
+    form_model = models.Player
+
+    form_fields = [
+        'q_CT_publicsector',
+        'q_CT_familyimportance',
+        'q_CT_wagevshours',
+        'q_CT_incomecontribution',
+    ]
+
+
+class Statements2(Page):
+    form_model = models.Player
+
+    form_fields = [
+        'q_CT_jobwomenindependence',
+        'q_CT_workingmother',
+        'q_CT_fatherSuited',
+        'q_CT_preschoolWork',
+    ]
+
+
 class Results(Page):
     pass
 
@@ -42,6 +116,16 @@ page_sequence = [
     RiskAversion,
     RiskPreference,
     MathLevel,
+    MajorDeclarationYN,
+    ExpectedStudyTrack,
+    DeclaredStudyTrack,
+    GPAStudyTrack,
+    ProfitableStudyTrack,
+    # MathLevelNYUAD,
+    # PassingGradeMath,
+    # Wealth,
+    Statements1,
+    Statements2,
     ResultsWaitPage,
     Results
 ]
