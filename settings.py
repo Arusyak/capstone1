2201181f37acb015df1c3d978a8caf4a6ff184e8
Laine <lum202@nyu.edu>
2018-02-13 14:10:23 +0400

    ret_timer to 300

--- settings.py
+++ settings.py
@@ -115,7 +115,7 @@ SESSION_CONFIGS = [
         'app_sequence': [
             'intro', 'ret1', 'ret2', 'ret3', 'survey'
         ],
-        'ret_timer': 60,
+        'ret_timer': 300,
         'showupfee': 30,
     },
     {
